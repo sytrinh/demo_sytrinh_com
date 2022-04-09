719ad0777009fa7a9e433c6d151e22695db07b0b
Sy Trinh <sytrinh89@gmail.com>
2022-04-10 11:05:19 +1200

    Prepare fro Heroku deployment

--- wsgi.py
+++ wsgi.py
@@ -2,8 +2,9 @@ from flaskr import create_app
 
 if __name__ == '__main__':
     app = create_app()
-    app.run(debug=True)
+    app.run()
+    # app.run(debug=True)
 else:
     gunicorn_app = create_app()
 
-# gunicorn --bind 127.0.0.1:5000 appserver:gunicorn_app
+# gunicorn --bind 127.0.0.1:5000 wsgi:gunicorn_app
